e95292050c8f81ce15f56f1bc8acc4b58bdfe5ea
Emre Kağan Erde <emrekaganerde@outlook.com>
2025-03-11 14:38:38 +0300

    Complete all tasks

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+import org.example.model.*;
+import org.example.model.enums.LampType;
+import org.example.model.enums.PaintColor;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Bedroom bedroom = new Bedroom("masterBedroom",
+                new Room(
+                        new Wall("North"),
+                        new Wall("South"),
+                        new Wall("East"),
+                        new Wall("West"),
+                        new Ceiling(4, PaintColor.GREEN),
+                        new Bed("Çift Kişilik", 5, 2, 3, 2),
+                        new Lamp(LampType.NEON, false, 90),
+                        new Wardrobe(3, 2, 50),
+                        new Carpet(4, 6, PaintColor.WHITE))
+        );
+
     }
 }
---
+++ src/main/java/org/example/model/Bedroom.java
@@ -0,0 +1,18 @@
+package org.example.model;
+
+public class Bedroom {
+    private String name;
+    private Room room;
+
+
+    public Bedroom(String name, Room room) {
+        this.name = name;
+        this.room = room;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+}
